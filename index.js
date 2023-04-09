039ae760913456bee0bb7f254fa1b2ec4387848d
Caio Sousa <jorelcarvalhosousa@gmail.com>
2023-04-08 21:21:35 -0300

    implementação da barra de navegação

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let Express = require('express');
 
 let app = Express();
-
+app.use(Express.static (__dirname + "/src"));
 app.get ("/" , (req, res) => {
-    res.sendFile(__dirname + '/src/index.html')
+    res.sendFile(__dirname + '/src/index.html');
 }  )
 
 app.listen(8081, () => {
